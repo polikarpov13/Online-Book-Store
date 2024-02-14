efed1515ae6c63e899e2f13215bc442fc9d22c72
Vlad Polikarpov <vladpolikarpovvp@gmail.com>
2024-02-15 00:17:49 +0200

    fixing the problems

--- src/test/resources/database/book/insert-controller-book.sql
+++ src/test/resources/database/book/insert-controller-book.sql
@@ -1,2 +1,2 @@
-INSERT INTO books(id, title, author, isbn, price, description, cover_image, is_deleted)
+INSERT INTO books(id, title, author, isbn, price, description, image, is_deleted)
 VALUES(13, 'Title', 'Author', 'ISBN', 13.00, 'Description', 'Image', false);
