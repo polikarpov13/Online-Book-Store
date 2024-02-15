53c7c0927a98dce420b8500ba4130cd7dc026692
Vlad Polikarpov <vladpolikarpovvp@gmail.com>
2024-02-15 22:08:38 +0200

    fixing problems

--- src/test/resources/database/book/remove-all-books-and-categories.sql
+++ src/test/resources/database/book/remove-all-books-and-categories.sql
@@ -1,3 +1,3 @@
 DELETE FROM books_categories;
-DELETE FROM books WHERE EXISTS (SELECT * FROM books WHERE id = 1);
-DELETE FROM categories WHERE EXISTS (SELECT * FROM categories WHERE id = 1);
+DELETE FROM books;
+DELETE FROM categories;
