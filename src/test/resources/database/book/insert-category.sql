db19d3af1252b94e82a28e6ca24fd20a20523cb7
Vlad Polikarpov <vladpolikarpovvp@gmail.com>
2024-02-15 10:50:20 +0200

    fixing problems

--- src/test/resources/database/book/insert-category.sql
+++ src/test/resources/database/book/insert-category.sql
@@ -1,2 +1,2 @@
-INSERT INTO categories(id, name, description, is_deleted)
-VALUES(1, 'Category', 'Description', false);
+INSERT INTO categories (id, name, description, is_deleted)
+VALUES (1, 'Category', 'Description', false);
